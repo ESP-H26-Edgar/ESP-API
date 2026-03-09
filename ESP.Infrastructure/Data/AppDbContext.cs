d97189eb66d7a05a1d09e95df77ab164477ebfe3
Edgar Gegout <2435196@cegeprdl.ca>
2026-03-09 10:07:16 -0400

    modification appdbcontext mysql

--- ESP.Infrastructure/Data/AppDbContext.cs
+++ ESP.Infrastructure/Data/AppDbContext.cs
@@ -30,7 +30,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=raceportal;uid=root;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("12.0.2-mariadb"));
+        => optionsBuilder.UseMySql("server=localhost;port=3306;database=raceportal;uid=apiuser;password=ApiPass10!", Microsoft.EntityFrameworkCore.ServerVersion.Parse("12.0.2-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
