7b055bb2a01573f74d75f70447e5c53e72563675
Edgar Gegout <2435196@cegeprdl.ca>
2026-04-13 15:09:55 -0400

    Fix : mauvais nom de table

--- ESP.Domain/Entities/Racetype.cs
+++ ESP.Domain/Entities/Racetype.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Stripe;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ESP.Infrastructure;
 
+[Table("RaceTypes")]
 public partial class Racetype
 {
     public int IdRaceType { get; set; }
 
+    [Column("RaceType")]
     public string RaceType1 { get; set; } = null!;
 
     public virtual ICollection<Race> Races { get; set; } = new List<Race>();
