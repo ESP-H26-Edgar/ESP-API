225f37fc5d58857804ad480d9c19a488f90fc604
Edgar Gegout <2435196@cegeprdl.ca>
2026-04-27 14:45:57 -0400

    Fix :Edit des messages d'erreurs

--- ESP.Application/Validators/RegisterValidator.cs
+++ ESP.Application/Validators/RegisterValidator.cs
@@ -10,16 +10,27 @@ public class RegisterValidaton : AbstractValidator<RegisterDto>
 {
     public RegisterValidaton()
     {
-        RuleFor(x => x.mail).NotEmpty().EmailAddress();
+        RuleFor(x => x.mail).NotEmpty().EmailAddress().WithMessage("L'adresse mail est requise");
         RuleFor(x => x.password)
-           .NotEmpty()
-           .MinimumLength(12)
-           .Matches("[A-Z]").WithMessage("Le mot de passe doit contenir au moins une majuscule")
-           .Matches("[0-9]").WithMessage("Le mot de passe doit contenir au moins un chiffre")
-           .Matches("[^a-zA-Z0-9]").WithMessage("Le mot de passe doit contenir un caractère spécial");
+         .Cascade(CascadeMode.Stop)
 
-        RuleFor(x => x.firstName).NotEmpty();
-        RuleFor(x => x.lastName).NotEmpty();
-        RuleFor(x => x.nationality).NotEmpty();
+         .NotEmpty()
+         .WithMessage("Le mot de passe est requis")
+
+         .MinimumLength(12)
+         .WithMessage("Le mot de passe doit contenir au moins 12 caractères")
+
+         .Matches("[A-Z]")
+         .WithMessage("Le mot de passe doit contenir une majuscule")
+
+         .Matches("[0-9]")
+         .WithMessage("Le mot de passe doit contenir un chiffre")
+
+         .Matches("[^a-zA-Z0-9]")
+         .WithMessage("Le mot de passe doit contenir un caractère spécial");
+
+        RuleFor(x => x.firstName).NotEmpty().WithMessage("Le nom est requis");
+        RuleFor(x => x.lastName).NotEmpty().WithMessage("Le prénom est requis");
+        RuleFor(x => x.nationality).NotEmpty().WithMessage("La nationnalité est requise");
     }
 }
